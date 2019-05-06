fccdd7f21490ef7b48e2231c6e66e872365baada
Yasmine <y.hartung@gmail.com>
2019-05-06 08:57:45 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(attr={})
+
+    net=Network.find_or_create_by(call_letters: attr[:call_letters])
+    self.network_id = net.id
+    net
+
+  end
 end
